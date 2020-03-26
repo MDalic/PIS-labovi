65a324b89483bcf87bb8faa5943114db892685c9
mdalic <steamskyapples@yahoo.com>
2020-03-26 14:28:26 +0100

    modified:   laboratorjske vjezbe 1/Program.cs

--- laboratorjske vjezbe 1/Program.cs
+++ laboratorjske vjezbe 1/Program.cs
@@ -7,6 +7,8 @@ namespace laboratorjske_vjezbe_1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+
         }
     }
 }
